e35dd66c293bd202bc481f54357245cc1163f981
Hussain Almajed <almajedhs@Hussains-MacBook-Pro.local>
2018-01-29 02:32:01 -0500

    almost done, only need the search bar

--- Yelp/BusinessesViewController.swift
+++ Yelp/BusinessesViewController.swift
@@ -8,17 +8,41 @@
 
 import UIKit
 
-class BusinessesViewController: UIViewController {
+class BusinessesViewController: UIViewController,UITableViewDataSource,UITableViewDelegate {
     
     var businesses: [Business]!
+     var searchBar = UISearchBar()
+    var searchController = UISearchController()
+    
+    
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 120
+       
+        searchBar.sizeToFit()
+        navigationItem.titleView = searchBar
+//        searchDisplayController?.displaysSearchBarInNavigationBar = true
+//        
+//        searchController.searchBar.sizeToFit()
+//       navigationItem.titleView = searchController.searchBar
+//        searchController.hidesNavigationBarDuringPresentation = false
+        
+        //searchController.hidesNavigationBarDuringPresentation = false
+        
+        
+        
         Business.searchWithTerm(term: "Thai", completion: { (businesses: [Business]?, error: Error?) -> Void in
             
             self.businesses = businesses
+            self.tableView.reloadData()
             if let businesses = businesses {
+                
                 for business in businesses {
                     print(business.name!)
                     print(business.address!)
@@ -27,7 +51,28 @@ class BusinessesViewController: UIViewController {
             
             }
         )
+    }
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            
+            if businesses != nil{
+                return businesses!.count
+            }else{
+                return 0
+            }
+            
+            
+        }
         
+         func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            
+            let cell = tableView.dequeueReusableCell(withIdentifier: "BusinessCell", for: indexPath) as! BusinessCell
+            
+            cell.business = businesses[indexPath.row]
+            
+            return cell
+            
+            
+        }
         /* Example of Yelp search with more search options specified
          Business.searchWithTerm("Restaurants", sort: .distance, categories: ["asianfusion", "burgers"], deals: true) { (businesses: [Business]!, error: Error!) -> Void in
          self.businesses = businesses
@@ -39,7 +84,7 @@ class BusinessesViewController: UIViewController {
          }
          */
         
-    }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
